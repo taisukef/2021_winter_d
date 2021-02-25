db54e093714f9b98248a2007fbada7553c0f4801
Genshi0916 <genshi0916@gmail.com>
2021-02-25 20:01:14 +0900

    :sparkles: ヘッダーの追加とフッターの追加と不要なファイルの削除

---
+++ components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-footer
+    app
+    dark
+  >
+    <div class="flex-grow-1"></div>
+    <div>© 2021 チーム hogehoge</div>
+  </v-footer>
+</template>
+
+<style>
+#gmail{
+  margin-right:3px;
+}
+</style>
