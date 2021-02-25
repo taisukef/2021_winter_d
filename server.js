eca9b99c65874c8842dea50f59e484135e6dde13
Genshi0916 <genshi0916@gmail.com>
2021-02-25 16:35:11 +0900

    :tada: initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+import { Server } from "https://js.sabae.cc/Server.js"
+  const list = []
+  class MyServer extends Server {
+    api(path) {
+      list.push(path)
+      return list
+    }
+  }
+new MyServer(8001);
