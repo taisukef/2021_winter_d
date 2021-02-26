b4f4c51886e14f5d196de34e6bab17c930504993
Genshi0916 <genshi0916@gmail.com>
2021-02-26 10:01:33 +0900

    :sparkles: Postとバズりの表示画面を追加

---
+++ components/Bazuri.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-card
+    class="mx-auto"
+    light
+  >
+    <v-timeline
+      align-top
+      dense
+    >
+      <v-timeline-item
+        v-for="(account,i) in accounts"
+        :key="i"
+        small
+        right
+      >
+        <v-card
+          class="mx-auto"
+          color="#26c6da"
+          dark
+        >
+          <v-card-actions>
+            <v-list-item class="grow">
+              <v-list-item-avatar color="grey darken-3">
+                <v-img
+                  class="elevation-6"
+                  :src="account.src"
+                ></v-img>
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title class="font-weight-bold">用意した名前</v-list-item-title>
+              </v-list-item-content>
+              <v-row
+                align="center"
+                justify="end"
+              >
+                <v-btn icon>
+                  <v-icon class="mr-1">mdi-heart</v-icon>
+                </v-btn>
+                <span class="subheading mr-2">{{account.likeNum}}</span>
+                <v-btn icon>
+                  <v-icon class="mr-1">mdi-share-variant</v-icon>
+                </v-btn>
+                <span class="subheading">{{account.shareNum}}</span>
+              </v-row>
+            </v-list-item>
+          </v-card-actions>
+          <v-card-text class="headline font-weight-bold">
+            {{account.comment}}
+          </v-card-text>
+        </v-card>
+      </v-timeline-item>
+    </v-timeline>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    data:() => ({
+      accounts:[
+        {
+          src:'https://avataaars.io',
+          name:'hoge',
+          comment:'用意したコメント',
+          likeNum:2,
+          shareNum:0,
+        },
+        {
+          src:'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light',
+          name:'hoge',
+          comment:'用意したコメント',
+          likeNum:1,
+          shareNum:1,
+        },
+      ]
+    }),
+  }
+</script>
---
+++ components/Post.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <v-card dark>
+      <div id="contactMain">
+        <h1 align="center" id="title">Bazらせたい作品を投稿！！</h1>
+        <v-container style="max-width: 800px;">
+          <v-card elevation="10">
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12">
+                  <v-file-input
+                    label="作品画像"
+                    multiple
+                    accept="image/png, image/jpeg"
+                    prepend-icon="mdi-image"
+                    v-model="files"
+                  >
+                    <template v-slot:selection="{ text }">
+                      <v-chip
+                        small
+                        label
+                        color="primary"
+                      >
+                        {{ text }}
+                      </v-chip>
+                    </template>
+                  </v-file-input>
+
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="6">
+                    <v-text-field label="作品タイトル" required v-model="title"></v-text-field>
+                  </v-col>
+                  <v-col cols="6">
+                    <v-text-field label="製作者名" required v-model="name"></v-text-field>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-textarea label="概要" outlined required v-model="description"></v-textarea>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn  v-on:click="submit" color="primary">Send</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-container>
+      </div>
+    </v-card>
+  </div>
+</template>
+<script>
+  export default{
+    components:{
+    },
+
+    data:()=>({
+      title:'',
+      name:'',
+      description:'',
+      files:[],
+    }),
+
+    methods:{
+      submit:function(){
+
+      }
+    }
+  }
+</script>
+<style scoped>
+  #contactMain{
+    padding-bottom: 30px;
+  }
+
+  #title{
+    padding:20px 0px;
+  }
+
+  a{
+    text-decoration: none;
+  }
+
+  @media screen and (min-width:0px) and ( max-width:480px){
+    #title{
+      font-size: 25px;
+    }
+  }
+</style>
