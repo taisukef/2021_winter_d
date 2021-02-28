8cc359a8a40ea455a834ae0e8e4bbf10b143858c
Taisuke Fukuno <fukuno@jig.jp>
2021-02-28 20:27:55 +0900

    サーバーを使わない表示するバージョン

--- pages/view/index.vue
+++ pages/view/index.vue
@@ -18,8 +18,15 @@
   </div>
 </template>
 <script>
-  import { fetchJSON } from "~/vendor/fetchJSON.js"
   import MainCard from '~/components/MainCard.vue'
+  
+  const staticData = 		{
+			"image_url": "http://localhost:8001/data/20210227/4b69cb3a-a13d-458a-b76a-152d9415bf38.jpeg",
+			"title": "Geo3x3 まだ緯度経度で消耗しているの！？",
+			"user_name": "@taisukef",
+			"description": "地球を東(E)と西(W)に分け、再帰的に3x3の9エリアで分割します。長いほど精度が上がります。"
+		};
+
 
   export default{
     components:{
@@ -27,8 +34,11 @@
     },
 
     async asyncData() {
-      const resp = await fetchJSON("http://localhost:8001/api/get_bazuri", {});
-      const respBody = await resp.json()
+      //const resp = await fetchJSON("http://localhost:8001/api/get_bazuri", {});
+      //const resp = await fetch("http://localhost:51236/db.json");
+      //const respBody = (await resp.json()).timeline[0];
+      const respBody = staticData;
+      console.log(respBody);
 
       const viewData = {
         items: [
